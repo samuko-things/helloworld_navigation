6cdcdbfeaa097a11e1a22ac1ceff36fc0aee09db
samuko-things <samuel.c.agba@gmail.com>
2026-09-11 01:14:10 +0100

    los skip working - needs more tests

--- robot_navigation/launch/robot_navigation.launch.py
+++ robot_navigation/launch/robot_navigation.launch.py
@@ -52,7 +52,7 @@ def generate_launch_description():
         name='test_planner',
         output='screen',
         parameters=[
-          {'planner_id': 0},
+          {'planner_id': 1},
           {'dist_to_obstacle_check': 0.25}
         ],
       )
